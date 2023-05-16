0562b1afd3ac9c42b31b0df1c7eeb8a17d0afe7d
Alexandre Marra <alexsmarra@hotmail.com>
2023-05-16 14:54:38 -0300

    instalando o axios

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+/* Para trabalhar com requisições http, para conseguir consumir requisições com post, get, 
+delete, etc.  */
+import axios from 'axios'
+
+export const api = axios.create({
+   /* endereço que está rodando em nosso computador, quando precisamos de algumas informação do 
+   backend, precisamos desse endereço, seria o nosso servidor, nesse caso. Quando precisarmos de 
+   alguma rota, basta usarmos por exemplo: api.get(/users/id) */
+   baseURL: 'http://localhost:3333'
+})
