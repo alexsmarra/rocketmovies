7da20f3ba8e6987ee9a76dae7a214364424fb014
Alexandre Marra <alexsmarra@hotmail.com>
2023-05-30 15:31:35 -0300

    update detalhes irrelevantes do codigo

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -87,7 +87,7 @@ export function SignUp() {
                />
 
                <Button 
-                  title='Entrar'
+                  title='Criar'
                   onClick={handleSignUp}
                />
             </div>
