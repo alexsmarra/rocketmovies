f7bc3a6bfa280b75f9bf68e38e60ad21c503d519
Alexandre Marra <alexsmarra@hotmail.com>
2023-05-30 14:42:44 -0300

    colocando endereco do deploy do backend em nosso arquivo api .js

--- src/services/api.js
+++ src/services/api.js
@@ -6,5 +6,5 @@ export const api = axios.create({
    /* endereço que está rodando em nosso computador, quando precisamos de algumas informação do 
    backend, precisamos desse endereço, seria o nosso servidor, nesse caso. Quando precisarmos de 
    alguma rota, basta usarmos por exemplo: api.get(/users/id) */
-   baseURL: 'http://localhost:3333'
+   baseURL: 'https://rocketmovies-api-kiv3.onrender.com'
 })
